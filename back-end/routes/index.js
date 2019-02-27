7fd5702e78e9c6892a30678c3a713fb1bcf88735
Katie Duane <katiejduane@gmail.com>
2019-02-26 19:10:36 -0500

    first queries

--- back-end/routes/index.js
+++ back-end/routes/index.js
@@ -1,9 +1,32 @@
 var express = require('express');
 var router = express.Router();
+const passport = require('passport');
+const pgp = require('pg-promise')();
+const config = require('../config');
+const connection = config.pg;
+const db = pgp(connection);
+
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/auth/github', passport.authenticate('github'));
+ 
+router.get('/auth/github/callback', passport.authenticate('github'), (req, res) => {
+  const selectQuery = `SELECT * FROM users WHERE username = $1`;
+  const username = req.user.username;
+  db.query(selectQuery, [username]).then((data)=>{
+    if(data.length === 0) {
+      const insertQuery = `INSERT INTO users(username)
+      VALUES ($1)`
+      db.query(insertQuery, [username]).then((results)=>{
+        
+      })
+    }
+    console.log(data, [username]);
+    res.json(data);
+  }).catch((error)=>{
+    res.json(error)
+  })
+  // res.json(req.user);
+})
 
 module.exports = router;
